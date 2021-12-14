6a8035db1d767da174e460e0e65cdac008429c64
root <root@NTBICSP375YQD3.localdomain>
2021-12-14 16:09:14 -0300

    Another change

--- cloud/oci/scripts/list_bv_bkp_policy_assmt.sh
+++ cloud/oci/scripts/list_bv_bkp_policy_assmt.sh
@@ -8,7 +8,7 @@ ad=$(oci --profile $profile iam availability-domain list | jq '.data[] | .name' 
 
 # Listar todos os OCID de todos os compartments ativos e salvar no arquivo comaprtmentList
 oci --profile $profile iam compartment list --compartment-id-in-subtree TRUE --all --lifecycle-state ACTIVE | jq '.data[] | .id' | tr -d \" > compartmentList
-file="compartmentList"
+compartmentList="compartmentList"
 
 # Listar todos os OCID dos boot-volumes de todos os compartments e salvar no arquivo bvList
 while read line
@@ -16,7 +16,7 @@ do
   outfile=$(echo $line)
   oci --profile $profile bv boot-volume list -c $line --availability-domain $ad | jq '.data[] | .id' | tr -d \" >> bvList
   oci --profile $profile bv volume list -c $line | jq '.data[] | .id' | tr -d \" >> vList
-done < "$file"
+done < "$compartmentList"
 
 # Listar todas os OCID das as atribuções de politica de backup dos Boot Volumes e salvar no arquivo policyAssigment
 file="bvList"
@@ -39,3 +39,5 @@ do
 done < "$file"
 
 diff vList policyAssetBlockVolume | grep ocid | tr -d \< > vWithoutPolicy
+
+# oci compute boot-volume-attachment list -c $willian --availability-domain FOjF:SA-SAOPAULO-1-AD-1
